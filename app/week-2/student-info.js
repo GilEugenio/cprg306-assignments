af46c0907478f8d72a729f0390ed5569af477b93
Gilmeugenio <Guillermo.Eugenio@edu.sait.ca>
2024-05-27 09:48:26 -0600

    4th commit

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,10 +3,8 @@ import Link from "next/link"
 export default function StudentInfo() {
     return (
         <div>
-            <h1 className="flex min-h-screen flex-col items-center justify-between p-24">Gil Eugenio</h1>
-            <Link href="https://vercel.com/guillermo-eugenios-projects/cprg306-assignments">
-                Github
-            </Link>
+            <h1>Gil Eugenio</h1>
+            <Link href="https://vercel.com/guillermo-eugenios-projects/cprg306-assignments">Github</Link>
 
         </div>
     )
