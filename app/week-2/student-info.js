9f7e5140fd8459a4aff2b612cf1733ce78d3eac9
Gilmeugenio <Guillermo.Eugenio@edu.sait.ca>
2024-05-27 09:19:54 -0600

    first commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>Gil Eugenio</h1>
+            <Link href="">
+                <h2>Github</h2>
+            </Link>
+
+        </div>
+    )
+}
